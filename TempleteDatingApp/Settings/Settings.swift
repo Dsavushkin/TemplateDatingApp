bb0d8343075d81f4281797e350535b039d8ec2ff
Dsavushkin <52749424+Dsavushkin@users.noreply.github.com>
2021-03-07 15:17:24 +0400

    extension UIColor and created Settings
    extension UIColor and created Settings

---
+++ TempleteDatingApp/Settings/Settings.swift
@@ -0,0 +1,22 @@
+//
+//  Settings.swift
+//  TempleteDatingApp
+//
+//  Created by Дмитрий on 07.03.2021.
+//
+
+import Foundation
+import UIKit
+
+enum Settings {
+    case main
+}
+
+struct MainSettings{
+        
+    static let generalColor = UIColor(hexString: "#8EA9FB") //8E21FB pink - 8EA9FB
+    static let logotype =  UIImage(systemName: "pencil")
+
+    
+}
+
--- TempleteDatingApp/ViewController.swift
+++ TempleteDatingApp/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = MainSettings.logotype
+        view.backgroundColor = MainSettings.generalColor
     }
 
 
